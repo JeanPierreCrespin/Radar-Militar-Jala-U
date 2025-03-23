fc1b402db1cbcfd18ee420af9e8437cb04744305
Jean Pierre Crespin <crespinpierre98@gmail.com>
2025-03-23 10:27:52 -0300

    se modifica el cmonnete radar militar para  poder tener para renderizar todos los niveles y se agrega logica para el movimiento por cada nivel

--- radar-militar/src/app/app.routes.ts
+++ radar-militar/src/app/app.routes.ts
@@ -1,4 +1,7 @@
 import { Routes } from '@angular/router';
 import { RadarMilitarComponent } from '../core/radar-militar/radar-militar.component';
 
-export const routes: Routes = [{path:'',component:RadarMilitarComponent}];
+export const routes: Routes = [
+  { path: '', redirectTo: 'radar/1', pathMatch: 'full' }, // 👈 Redirige la raíz a nivel2/1
+  { path:'radar/:level',component:RadarMilitarComponent },
+];
